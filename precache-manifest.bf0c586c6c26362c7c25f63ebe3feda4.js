e3c6ee24423b924aa39553e62315cab8f2e44032
Roger Man <rman@deloitte.co.uk>
2020-05-16 01:38:22 +0100

    Updates

---
+++ precache-manifest.bf0c586c6c26362c7c25f63ebe3feda4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbfd63b6f6945736881a5cdfa060ab7a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b6f939a7e8a3f76afb95",
+    "url": "/monsters-rolodex/static/css/main.bddc414c.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f939a7e8a3f76afb95",
+    "url": "/monsters-rolodex/static/js/main.efe7efd4.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
